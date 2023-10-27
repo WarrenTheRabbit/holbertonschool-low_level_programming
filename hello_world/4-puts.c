8dca67ffea21d026ff31dfcfefdd6fbbe981feaa
root <rabbitsinwarrens@gmail.com>
2023-10-27 04:18:18 -0700

    feat: add task 4 for C-Hello_World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 # include <stdio.h>
 
-int main() {
+/**
+ * main - Short description, single line
+ * Return: Description of the returned value
+ */
+int main(void)
+{
 	char message[] = "Programming is like building a multilingual puzzle";
+
 	puts(message);
-	return 0;
+	return (0);
 }
