b684ef7b1d0516e18f536777b03f5a89f20a824f
root <rabbitsinwarrens@gmail.com>
2023-10-29 15:51:43 -0700

    feat: add task 1 for C-Variables,if,else,while
    This program checks multiple conditions to determine
    what should be printed to standard out. Is it worth
    refactoring the conditional logic to functions that
    return a bool value? See Martin Fowler.

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int get_last_digit(int number);
+
+/**
+ * main - Entry point of program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int number, last_digit;
+
+	srand(time(0));
+	number = rand() - RAND_MAX/2;
+	last_digit = get_last_digit(number);
+	
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", number, last_digit);
+	}
+	else if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", number, last_digit);
+	}
+	else 
+	{
+		printf("Last digit of %d is %d and is 0\n", number, last_digit);
+	}
+
+	return (0);
+}
+
+
+/**
+ * get_last_digit - returns last digit of an integer and retains original number's sign.
+ * @number: The integer to extract the last digit and sign from.
+ * Return: Last digit and sign of `number` such that the digits `abc` 
+ * returns `c` and `-abc` returns `-c`.
+ */
+int get_last_digit(int number)
+{
+	int last_digit = number % 10;
+
+	return (last_digit);
+}
