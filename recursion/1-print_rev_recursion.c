79805ef121e044f7b7a74046ed6539e2fa63f130
root <rabbitsinwarrens@gmail.com>
2023-10-29 21:05:32 -0700

    refactor: wrap recursion functionto print endline

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
 #include "main.h"
+
+
+void _reverse_recurse(char *s);
 
 /**
  * _print_rev_recursion - prints string in reverse
@@ -7,14 +11,25 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
-	else
-	{
-		_print_rev_recursion(++s);
-		_putchar(*s);
-	}
+	_reverse_recurse(s);
+	_putchar('\n');
+}
 
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string to be reversed
+ *
+ */
+void _reverse_recurse(char *s) 
+{
+        if (*s == '\0')
+        {
+                return;
+        }
+        else
+        {
+                _reverse_recurse(++s);
+                _putchar(*s);
+        }
 }
