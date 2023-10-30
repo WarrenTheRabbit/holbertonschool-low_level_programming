0526a75f4327df200e02d47005c2b696a8c722d9
root <rabbitsinwarrens@gmail.com>
2023-10-29 21:46:29 -0700

    feat: add task 2 for recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string whose lenght is to be determined.
+ * Return: Length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 +  _strlen_recursion(s + 1));
+	}
+}
