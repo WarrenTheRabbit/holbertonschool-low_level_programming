6236a8fc183b0b7aa7480b77b54c5048b60dc866
root <rabbitsinwarrens@gmail.com>
2023-10-30 13:19:14 -0700

    fix: conform to Betty style

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,7 +4,6 @@
 /**
  * _sqrt_recursion - calculates x raised to y
  * @n: base
- * @y: exponent
  * Return: n to the power of y
  */
 int _sqrt_recursion(int n)
@@ -12,17 +11,22 @@ int _sqrt_recursion(int n)
 	return (_do_calculation(n, 1));
 }
 
-
+/**
+ * _do_calculation - calculates x raised to y
+ * @n: number
+ * @y: number_to_square
+ * Return: n to the power of y
+*/
 int _do_calculation(int n, int y)
 {
-        if (y * y == n)
-        {
-                return (y);
-        }
-        if (y * y > n)
-        {
-                return (-1);
-        }
+	if (y * y == n)
+	{
+	return (y);
+	}
+	if (y * y > n)
+	{
+	return (-1);
+	}
 
-        return (_do_calculation(n, y + 1));
+	return (_do_calculation(n, y + 1));
 }
