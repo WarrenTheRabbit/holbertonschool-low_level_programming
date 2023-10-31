156fc8613d4c3de751c505328348396f19c3b35a
root <rabbitsinwarrens@gmail.com>
2023-10-30 21:00:21 -0700

    refactor: remove debugging statements

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -14,29 +14,19 @@ int _is_palindrome_recursive(char *str, int call_number)
 {
 	int size = strlen(str);
 	int substring_size = size - (2 * call_number);
+	char first_character = *(str + call_number);
+	char last_character = *(str + call_number + substring_size - 1);
 
 	if (substring_size <= 1)
 	{
 		return (1);
 	}
-
-	printf("The string is %s\n",str);
-
+	else if (first_character != last_character)
 	{
-		char first_character = *(str + call_number);
-		char last_character = *(str + call_number + substring_size - 1);
-
-
-		printf("\tThe first character is %c\n", first_character);
-		printf("\tThe last character is %c\n\n", last_character);
-		
-
-		if (first_character != last_character)
-		{
-			printf("Not a palindrome.");
-			return (0);
-		}
+		return (0);
 	}
-
-	return _is_palindrome_recursive(str, call_number + 1);
+	else 
+	{	
+		return _is_palindrome_recursive(str, call_number + 1);
+	}
 }
