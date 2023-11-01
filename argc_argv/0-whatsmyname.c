d556fef8311f3d77d336a2e0065aa96590448b35
root <rabbitsinwarrens@gmail.com>
2023-10-31 23:40:24 -0700

    style: remove trailing white space
    Used vim normal mode command:
       :%s/\s/+%//
    
    %s - starts substitution over whole file
    /\s\+$/ - \s (whitespace) \+ (one or more) $ (end position)
    // replacement pattern (empty)

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
  * main - entry point of program
  * @argc: the count of the command line arguments
  * @argv: an array of size `argc` that stores the command line arguments
- * 
+ * Return: Always return success message.
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;	
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (EXIT_SUCCESS);
 }
