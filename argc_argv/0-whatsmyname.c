2e1e510937b430efd3f6863ac42c0e4031b9ee2c
root <rabbitsinwarrens@gmail.com>
2023-10-31 23:15:40 -0700

    feat: add task 0 of args project
    Learned about (void) to silence unusaed variable warning.
    Made use of ALT + T to swap the position of two words
    on the command line.
    Observed that compile treats char as int.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of program
+ * @argc: the count of the command line arguments
+ * @argv: an array of size `argc` that stores the command line arguments
+ * 
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;	
+	printf("%s\n", argv[0]);
+	return (EXIT_SUCCESS);
+}
