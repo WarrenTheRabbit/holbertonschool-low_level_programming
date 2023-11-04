df9eff4a1ff23ffbfb4a8de20799f0597bb6d751
root <rabbitsinwarrens@gmail.com>
2023-11-03 19:20:09 -0700

    feat: add task 2 for argc_argv
    Used a while loop and increasing
    sentinel value.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ * @argc: count of arguments from the host environment
+ * @argv: array of pointers to strings passed into command line
+ * Return: Always EXIT_SUCCESS
+ */
+int main(int argc, char **argv)
+{
+	/**
+	 * The command line passes the name of the program
+	 * as the first argument. The index is set to 1 to
+	 * skip this unwanted program name argument.
+	 */
+	int index = 1;
+
+	while (index < argc)
+	{
+		printf("%s\n", argv[index++]);
+
+	}
+}
