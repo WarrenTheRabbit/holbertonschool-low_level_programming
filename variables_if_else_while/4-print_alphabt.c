3af02a51904f7ae4941f179e56a64cfee6ce0b8e
root <rabbitsinwarrens@gmail.com>
2023-11-06 02:28:26 -0800

    feat: add task 4 for variables

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+int is_permitted_character(char character);
+void print_lowercase_alphabt(void);
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	print_lowercase_alphabt();
+	putchar('\n');
+	return (EXIT_SUCCESS);
+}
+
+
+/**
+ * is_permitted_character - function
+ * @character: letter to check
+ * Return: 1 if permitted; 0 otherwise
+ *
+ */
+int is_permitted_character(char character)
+{
+	if (character == 'q' || character == 'e')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+
+/**
+ * print_lowercase_alphabt - entry point
+ * Return: 0
+ */
+void print_lowercase_alphabt(void)
+{
+	int start_letter = 'a';
+	int end_letter = 'z';
+	int current_letter = start_letter;
+
+	while (current_letter <= end_letter)
+	{
+		if (is_permitted_character(current_letter))
+		{
+			putchar(current_letter);
+		}
+
+	current_letter++;
+
+	}
+}
+
+/**
+ * print_uppercase_alphabet - entry point
+ * Return: 0
+ */
+void print_uppercase_alphabet(void)
+{
+	int start_letter = 'A';
+	int end_letter = 'Z';
+	int current_letter = start_letter;
+
+	while (current_letter <= end_letter)
+	{
+		putchar(current_letter);
+		current_letter++;
+	}
+}
