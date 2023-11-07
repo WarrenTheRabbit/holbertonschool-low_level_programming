813bc89761a2c2798526ef239fe08eec865546ad
root <rabbitsinwarrens@gmail.com>
2023-11-07 02:42:53 -0800

    feat: add task 1 for functions_loops

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_alphabet - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+	int start_letter = 'a';
+	int count = 0;
+
+	while (count < 26)
+	{
+		_putchar(start_letter + count);
+		count++;
+	}
+
+	_putchar('\n');
+}
