50d41872a2a974d0e3a2d5f942c434f2b29bb9d4
root <rabbitsinwarrens@gmail.com>
2023-11-08 13:08:33 -0800

    feat: add task 2 for functions,nested

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -14,8 +14,29 @@ void print_alphabet_x10(void)
 	while (count < 10)
 	{
 		print_alphabet();
-		_putchar('\n');
 		count++;
 	}
 
 }
+
+
+/**
+ * print_alphabet - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+        int start_letter = 'a';
+        int count = 0;
+
+        while (count < 26)
+        {
+                _putchar(start_letter + count);
+                count++;
+        }
+
+        _putchar('\n');
+}
+
