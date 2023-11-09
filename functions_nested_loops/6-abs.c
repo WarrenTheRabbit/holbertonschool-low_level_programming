f00bc2baf2071904ecc5cc44edc917fcb01999a3
root <rabbitsinwarrens@gmail.com>
2023-11-08 16:25:49 -0800

    feat: add task 6 for functions,ntested

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - writes the character c to stdout
+ * @n: input
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+
+}
