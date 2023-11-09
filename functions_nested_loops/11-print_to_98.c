861d90d8c3a03f92bfdf86aec6ec6b37ba4f18e0
root <rabbitsinwarrens@gmail.com>
2023-11-09 00:37:24 -0800

    feat: add task 11 for functions

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "main.h"
 
+void print_digits(int number);
+void print_98(void);
+void print_ascending_numbers(int n);
+void print_descending_numbers(int n);
+void print_number(int number);
+
 /**
  * print_to_98 - writes the character c to stdout
  * @n: number
@@ -9,12 +15,66 @@
  */
 void print_to_98(int n)
 {
-	int i;
-	for (i = n; i < 98, ++i)
+	if (n < 98)
 	{
-		_putchar('0' + i);
-		_putchar(',');
-		_putchar(' ');
+		print_ascending_numbers(n);
 	}
-	_putchar('0' + 98);
+	else if (n > 98)
+	{
+		print_descending_numbers(n);
+	}
+
+	print_98();
+	
+}
+
+void print_98(void)
+{
+	print_digits(98);
+	_putchar('\n');
+}
+
+
+void print_ascending_numbers(int n)
+{
+	int i;
+	for (i = n; i < 98; ++i)
+	{
+		print_number(i);
+	}
+}
+
+void print_descending_numbers(int n)
+{
+	int i;
+	for (i = n; i > 98; --i)
+	{
+		print_number(i);
+	}
+}
+
+void print_number(int number)
+{
+	int absolute = number; 
+
+	if (number < 0)
+	{
+		absolute = number * -1;
+		_putchar('-');
+	}
+
+	print_digits(absolute);
+	_putchar(',');
+	_putchar(' ');
+}
+
+void print_digits(int number)
+{
+	int terminal_digit = number % 10;
+
+	if (number >= 10)
+	{
+		print_digits(number / 10);
+	}
+	_putchar('0' + terminal_digit);
 }
