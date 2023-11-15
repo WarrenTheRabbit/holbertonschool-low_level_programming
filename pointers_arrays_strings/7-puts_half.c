4eb750a6152542da75350a760927bf3cbd84bbdc
root <rabbitsinwarrens@gmail.com>
2023-11-15 01:18:14 -0800

    feat: add task 7 for pointers

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+size_t _getsize(char *str);
+
+/**
+ * puts_half - entry point
+ * @str: pointer to char
+ */
+void puts_half(char *str)
+{
+	size_t size = _get_size(str);
+	size_t midpoint = (size + 1) / 2;
+	size_t i = midpoint;
+
+	while (i < size)
+	{
+		printf("%zu\n", i);
+	}
+}
+
+/**
+ * _get_size - returns size of string
+ * @str: pointer to char
+ * Return: size of string
+ */
+size_t _get_size(char *str)
+{
+	size_t count = 0;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+
+	return (count);
+}
+
+
