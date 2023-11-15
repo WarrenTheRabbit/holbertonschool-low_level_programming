54e2f01299985264948ebdde66628bb91ea66db1
root <rabbitsinwarrens@gmail.com>
2023-11-15 03:25:15 -0800

    feat: add task 10 for pointers

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _atoi - convert string to integer
+ * @s: pointer to a char
+ * Return: extracted integer from string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	size_t index = 0;
+	int number = 0;
+/** search for first digit and determine sign of number */
+	while (1)
+	{
+
+		if (s[index] == '-')
+		{
+			sign *= -1;
+		}
+
+		if (s[index] >= '0' && s[index] <= '9')
+		{
+			break;
+		}
+
+		index++;
+	}
+/**  accumulate characters into a digit */
+	while (1)
+	{
+		if (s[index] == '\0')
+		{
+			break;
+		}
+		/** terminate when non-digit character found. */
+		if (s[index] < 48 || s[index] > 57)
+		{
+			break;
+		}
+		number *= 10;
+		number += (s[index] - '0');
+
+		index++;
+
+	}
+
+	return (sign * number);
+}
+
+
+
