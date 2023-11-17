678f7261005198505d59d6f5bcf94b53b15cc78d
root <rabbitsinwarrens@gmail.com>
2023-11-17 14:05:15 -0800

    fix: handle null case

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-
+#include <stddef.h>
+#include <stdio.h>
 /**
  * init_dog - initialises a dog by reference
  * @d: dog struct
@@ -10,6 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
