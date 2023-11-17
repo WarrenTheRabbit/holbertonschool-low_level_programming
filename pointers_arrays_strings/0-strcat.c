c75f3a19bfb9de24c7e38d4dd04691c70bef9c9c
root <rabbitsinwarrens@gmail.com>
2023-11-16 23:34:05 -0800

    Task 0 for more pointeres

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strcat - description
+ * @dest: pointer to char
+ * @src: pointer to char
+ * Return: pointer to char
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	// Find end of dest string.
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	while (*src != '\0') 
+	{
+		*ptr++ = *src++;	
+	}
+
+
+	*ptr = '\0';
+
+	return (dest);	
+}
