5247e84326e568c6749d2bebfb59f048f91bfa00
root <rabbitsinwarrens@gmail.com>
2023-11-17 00:48:37 -0800

    feat: add task 2 for more pointers

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strncpy - description
+ * @dest: pointer to char
+ * @src: pointer to char
+ * @n: int
+ * Return: pointer to char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n ; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+
+}
