3cd08a02ccbd1aa63360cbdd656a392fc4721a37
root <rabbitsinwarrens@gmail.com>
2023-11-17 15:28:41 -0800

    style: remove spaces

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -5,11 +5,13 @@
  * free_dog - frees memory
  * @dog: dog data type
  */
-void free_dog(dog_t *dog) {
-    if (dog != NULL) {
-        free(dog->name);   
-        free(dog->owner); 
-        free(dog);
-    }
+void free_dog(dog_t *dog)
+{
+	if (dog != NULL)
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+	}
 }
 
