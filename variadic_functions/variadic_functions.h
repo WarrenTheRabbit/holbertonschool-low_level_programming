0ea3851b0e6e19c2c0795e7e4ed181b41ce5f528
root <rabbitsinwarrens@gmail.com>
2023-11-18 04:32:06 -0800

    chore: update prototypes

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,8 @@
 #define HEADER_H
 #include <stdarg.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -12,6 +14,13 @@ void print_int(va_list *args);
 void print_float(va_list *args);
 void print_string(va_list *args);
 void print_char(va_list *args);
+
+typedef void (*print_handler)(va_list *);
+
+typedef struct {
+    char type_id;
+    print_handler handler;
+} TypeHandler;
 
 /**
  * struct Formatter - Struct op
