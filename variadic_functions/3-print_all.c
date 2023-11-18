537b33db12d4f37638b5a64344dbee9b04ca13e2
root <rabbitsinwarrens@gmail.com>
2023-11-18 05:19:30 -0800

    style: remove spaces

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,14 +1,4 @@
 #include "variadic_functions.h"
-
-TypeHandler handlers[] = {
-    {'c', print_char},
-    {'s', print_string},
-    {'i', print_int},
-    {'f', print_float},
-    {0, print_nothing}
-};
-
-
 /**
  * print_all - sums all arguments
  * @separator: first argument
@@ -20,18 +10,26 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int index = 0;
 	int j = 0;
+TypeHandler handlers[] = {
+	{'c', print_char},
+	{'s', print_string},
+	{'i', print_int},
+	{'f', print_float},
+	{0, print_nothing}
+};
+
 	va_start(args, format);
 
 	while (format && format[index])
 	{
 		j = 0;
-		while (handlers[j].type_id != 0 && handlers[j].type_id != format[index]) 
+		while (handlers[j].type_id != 0 && handlers[j].type_id != format[index])
 		{
 			j++;
 		}
 		handlers[j].handler(&args);
-		index++;	
-		
+		index++;
+	
 		if (format[index] != '\0' && handlers[j].handler != print_nothing)
 			printf(", ");
 
@@ -50,29 +48,29 @@ void print_nothing(va_list *args)
 
 void print_int(va_list *args)
 {
-        int d = va_arg(*args, int);
-        printf("%d", d);
+	int d = va_arg(*args, int);
+	printf("%d", d);
 }
 
 void print_string(va_list *args)
 {
-        char *s = va_arg(*args, char *);
+	char *s = va_arg(*args, char *);
 	if (s == NULL)
 	{
 		s = "(nil)";
 	}
-        printf("%s", s);
+	printf("%s", s);
 }
 
 void print_char(va_list *args)
 {
-        char c = va_arg(*args, int);
-        printf("%c", c);
+	char c = va_arg(*args, int);
+	printf("%c", c);
 }
 
 void print_float(va_list *args)
 {
-        double f = va_arg(*args, double);
-        printf("%f", f);
+	double f = va_arg(*args, double);
+	printf("%f", f);
 }
 
