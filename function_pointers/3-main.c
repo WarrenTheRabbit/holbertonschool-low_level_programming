4520af3cf2b0216a75758e3c21450adfcda6738c
root <rabbitsinwarrens@gmail.com>
2023-11-17 19:08:24 -0800

    feat: add task 3 for function pointers
    The current implementation uses printf. This
    needs to be changed in a future commit as
    printf is disalowed by the requirements.
    
    The demonstrated program execution evaluates
    1024 % 98 as 44, whereas my calculator and
    the current solution evaluates to 45.

---
+++ function_pointers/3-main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+
+int main(int argc, char **argv)
+{
+	int operand1, operand2, result;
+	int (*operator)(int, int);
+
+	operand1 = atoi(argv[1]);
+	operand2 = atoi(argv[3]);
+	
+	operator = get_op_func(argv[2]);
+	if (operator == NULL) {
+    		printf("Error\n");
+		return (1);	
+	}
+
+	result = operator(operand1, operand2);
+	printf("%d\n", result);
+
+	return (0);
+}
