e6e283c48cdbb90d17b4ee342d4767864e813b87
root <rabbitsinwarrens@gmail.com>
2023-11-18 00:23:04 -0800

    style: remove spaces

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			next_arg = "(nil)";
 
 		printf("%s", next_arg);
- 
+
 		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
