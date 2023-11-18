42410245fa92253012d930e7f4613be0105614a3
root <rabbitsinwarrens@gmail.com>
2023-11-18 00:21:34 -0800

    feat: print null as nil

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,13 +13,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-
+	char *next_arg;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(args, char *));
+		next_arg = va_arg(args, char *);
+
+		if (next_arg == NULL)
+			next_arg = "(nil)";
+
+		printf("%s", next_arg);
+ 
 		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
