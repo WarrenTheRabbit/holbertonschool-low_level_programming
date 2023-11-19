e9d40ec47f2103530b5c14c3ae5e526e635df8c3
root <rabbitsinwarrens@gmail.com>
2023-11-18 17:55:05 -0800

    feat: pass test_that_allocates_memory

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * create_array - task 0
+ * @size - unsigned int
+ * @c - char
+ * Return - pointer to char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = NULL;
+	ptr = malloc(1);
+	return (ptr);
+}
+
