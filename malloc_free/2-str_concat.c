43e39fe0841cecc4caa2e1c63eead7124aa49e31
root <rabbitsinwarrens@gmail.com>
2023-11-27 03:35:56 -0800

    fix: add _strlen to module

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: pointer to char
+ * Return: lengh of string
+ */
+int _strlen(char const *s)
+{
+        char const *start = s;
+        char const *current = s;
+
+        while (*current)
+        {
+                current++;
+        }
+        return (current - start);
+}
 
 
 /**
