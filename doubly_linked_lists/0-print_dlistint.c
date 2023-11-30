6c96feea7b9f6d08764957b6792fcc0b4b823c81
root <rabbitsinwarrens@gmail.com>
2023-11-30 00:06:57 -0800

    feat: add task 0 for doubly-linked

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elements of doubly linked list
+ * @h: pointer to a doubly linked list node
+ * Return: the number of nodes in the doubly linked list
+ */
+size_t print_dlistint(dlistint_t const *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}	
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,44 @@
+
+#ifndef HEADER
+#define HEADER
+	#include <stddef.h>
+	#include <string.h>	
+	#include <stdlib.h>
+	#include <stdio.h>
+	typedef struct list_t list_t;
+
+	struct list_t 
+	{
+		char *str;		
+		unsigned int len;
+		list_t *next;
+	};
+
+	/**
+	 * struct dlistint_t - doubly linked list
+	 * @n: integer
+	 * @prev: points to the previous node
+	 * @next: points to the next node
+	 *
+	 * Description: doubly linked list node structure
+	 *
+	 */
+	typedef struct dlistint_t
+	{
+		int n;
+		struct dlistint_t *prev;
+		struct dlistint_t *next;
+	} dlistint_t;
+
+
+	size_t list_len(const list_t *h);
+	int _putchar(char c);
+	list_t *add_node(list_t **head, char *str);
+	size_t print_list(const list_t *h);
+	list_t *add_node_end(list_t **head, char const *str);
+	void free_list(list_t *head);
+	size_t print_dlistint(const dlistint_t *h);
+	size_t dlistint_len(const dlistint_t *h);
+	dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+#endif
+
