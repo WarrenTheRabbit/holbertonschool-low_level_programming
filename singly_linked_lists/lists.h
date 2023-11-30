f77fd79bb89b80c4cf4f085e975cf87d01a53068
root <rabbitsinwarrens@gmail.com>
2023-11-29 19:07:39 -0800

    feat: add task 4 for singly_linked

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -18,5 +18,6 @@
 	list_t *add_node(list_t **head, char *str);
 	size_t print_list(const list_t *h);
 	list_t *add_node_end(list_t **head, char const *str);
+	void free_list(list_t *head);
 #endif
 
