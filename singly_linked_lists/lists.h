e0d46b3c1cb93aa25d75ce960d4f7470a31b1e40
root <rabbitsinwarrens@gmail.com>
2023-11-29 16:22:26 -0800

    chore: update dependencies

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef HEADER
 #define HEADER
 	#include <stddef.h>
-		
-
+	#include <string.h>	
+	#include <stdlib.h>
 
 	typedef struct list_t list_t;
 
@@ -15,7 +15,8 @@
 
 	size_t list_len(const list_t *h);
 	int _putchar(char c);
-
+	list_t *add_node(list_t **head, char *str);
+	size_t print_list(const list_t *h);
 
 #endif
 
