068ad5833a3574b1d3fc2c2f4c5de1a9d9d22b78
root <rabbitsinwarrens@gmail.com>
2023-11-29 19:13:01 -0800

    style: describe free_list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * free_list - frees all memory in the linked list
+ * @head: pointer to a linked list node
+ * Returns: void
+ */
 void free_list(list_t *head)
 {
 	if (!head)
