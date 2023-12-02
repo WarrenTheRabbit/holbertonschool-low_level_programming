449142837f9764c70e3355997e8460365196a8ed
root <rabbitsinwarrens@gmail.com>
2023-12-02 03:25:20 -0800

    refactor: clarify insert cases

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,51 +15,65 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current_node = *h;
 	unsigned int current_index = 0;
 
-	if (!current_node && idx == 0)
-	{
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*h = new_node;
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 
+	/* Handle insertions that require no iteration first. 
+	 * - inserting a node at the start of an empty list
+	 * - inserting a node at the start of a nonempty list
+	 */
+
+	/* The start of an empty list */
+	if (!h && !idx)
+	{
+		*h = new_node;
+		return (new_node);
 	}
 
-	while (current_index < idx && current_node->next)
+
+	/* The start of a nonempty list */
+	if (h && !idx)
+	{
+		new_node->next = current_node;
+		current_node->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	/* Handle the cases that require iteration 
+	 * - between two nodes
+	 * - at the end of the list
+	 * - insertion point not in range
+	 */
+
+	while (current_node->next && (current_index + 1) < idx)
 	{
 		current_index++;
 		current_node = current_node->next;
 	}
 
-	if (current_index == 0)
-	{
-		new_node->n = n;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*h = new_node;
-		return (new_node);
-	}
 
-	if (current_index == idx)
+	/* Between two nodes */
+	if (current_node->next)
 	{
-		new_node->n = n;
-		new_node->next = current_node;
-		new_node->prev = current_node->prev;
-		current_node->prev->next = new_node;
-		current_node->prev = new_node;
+		new_node->next = current_node->next;
+		current_node->next->prev = new_node;
+		current_node->next = new_node;
+		new_node->prev = current_node;
 		return (new_node);
 	}
+	/* At the end of the list */
 	else if (current_index + 1 == idx)
 	{
-		new_node->n = n;
+		current_node->next = new_node;
 		new_node->prev = current_node;
-		new_node->next = NULL;
-		current_node->prev->next = new_node;
-		current_node->prev = new_node;
 		return (new_node);
 	}
+	/* Insertion point not in range */
 	else
 	{
-		free(new_node);
 		return (NULL);
 	}
 }
+
