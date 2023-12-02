d3e0101e3693dbed16da32b4e7ac1137e594ef8a
root <rabbitsinwarrens@gmail.com>
2023-12-02 00:30:58 -0800

    fix: handle inner links

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -53,7 +53,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->n = n;
 		new_node->prev = current_node;
 		new_node->next = NULL;
-		current_node->next = new_node;
+		current_node->prev->next = new_node;
+		current_node->prev = new_node;
 		return (new_node);
 	}
 	else
