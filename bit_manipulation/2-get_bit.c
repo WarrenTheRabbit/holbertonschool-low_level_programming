789759ad1f42c1ef7f8817d0e4847ba26bb2fb5d
root <rabbitsinwarrens@gmail.com>
2023-12-03 14:15:40 -0800

    feat: add task 2 for bit manipulation

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_nth_bit_set - description
+ * @number: number
+ * @n: index
+ * Return: 0 if not set else 1 or greater
+ */
+unsigned long int is_nth_bit_set(unsigned long int number, unsigned int n)
+{
+	return (number & 1 << n);
+}
+
+/**
+ * int get_bit - Gets the value of the bit at a given index.
+ * @n: number
+ * @index: index
+ * Return: 0 if not set else 1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (is_nth_bit_set(n, index))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
+
