7d005364e9a7e764119a575fd1bcd5be3eed5d0d
root <rabbitsinwarrens@gmail.com>
2023-12-03 15:29:07 -0800

    feat: task 5 for bit manipulation

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,36 @@
+
+#include "main.h"
+
+/**
+ * count_set_bits - counts number of bits
+ * @n: number to count the bits of
+ * Return: number of set bits
+ */
+int count_set_bits(int n)
+{
+	int count = 0;
+
+	while (n)
+	{
+		count += n & 1;
+		n >>= 1;
+	}
+
+	return (count);
+}
+
+/**
+ * flip_bits - Description.
+ * @n: n
+ * @m: m
+ * Return: number of bits you would need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int xor_result = n ^ m;
+
+	return (count_set_bits(xor_result));
+}
+
+
+
