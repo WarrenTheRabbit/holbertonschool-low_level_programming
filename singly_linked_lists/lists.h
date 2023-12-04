e52a1b3a2cd4be8aa06e30282f117f9bf805eed2
root <rabbitsinwarrens@gmail.com>
2023-12-03 17:05:28 -0800

    style: remove indents

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,23 +1,31 @@
 #ifndef HEADER
 #define HEADER
-	#include <stddef.h>
-	#include <string.h>
-	#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 
-	typedef struct list_t list_t;
+typedef struct list_t list_t;
 
-	struct list_t
-	{
-		char *str;
-		unsigned int len;
-		list_t *next;
-	};
+/**
+* struct list_t - A structure representing a linked list node.
+* @str: String data stored in the node.
+* @len: Length of the string.
+* @next: Pointer to the next node in the linked list.
+*/
+struct list_t
+{
+char *str;
+unsigned int len;
+list_t *next;
+};
+typedef struct list_t list_t;
 
-	size_t list_len(const list_t *h);
-	int _putchar(char c);
-	list_t *add_node(list_t **head, char *str);
-	size_t print_list(const list_t *h);
-	list_t *add_node_end(list_t **head, char const *str);
-	void free_list(list_t *head);
+size_t list_len(const list_t *h);
+int _putchar(char c);
+list_t *add_node(list_t **head, char *str);
+size_t print_list(const list_t *h);
+list_t *add_node_end(list_t **head, char const *str);
+void free_list(list_t *head);
+
 #endif
 
