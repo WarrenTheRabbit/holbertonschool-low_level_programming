2c133bb64004feadfb0da26324ce32c57c933e82
root <rabbitsinwarrens@gmail.com>
2023-12-04 16:05:35 -0800

    feat: task 0 using printf

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * read_textfile - function
+ * @filename: parameter
+ * @letters: parameter
+ * Return: number of read and printed bytes
+ */
+ssize_t read_textfile(char const *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t fd;
+	ssize_t n;
+
+	if (!filename)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
+	{
+		return (0);
+	}
+
+	n = read(fd, buffer, letters);
+
+	if (n == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	printf("%s", buffer);
+
+	close(fd);
+
+	return (n);
+	
+}
