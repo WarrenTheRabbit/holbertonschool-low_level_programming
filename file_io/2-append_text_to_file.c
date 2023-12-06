827c57d28f7929e6da60992a5035cf18f448151a
root <rabbitsinwarrens@gmail.com>
2023-12-05 18:03:53 -0800

    style: remove empty line

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,8 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	bytes_written = write(fd, text_content, strlen(text_content));
 
-	close(fd);
-	
+	close(fd);	
 	if (bytes_written == -1)
 	{
 		close(fd);
