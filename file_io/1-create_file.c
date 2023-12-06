a71d20292feaa5268ff12582c53130326caca968
root <rabbitsinwarrens@gmail.com>
2023-12-05 16:48:57 -0800

    styel: remove trailing space

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,14 +15,14 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, S_IRUSR | S_IWUSR);
 
-	if (fd == FILE_MAY_EXIST_ALREADY) 
+	if (fd == FILE_MAY_EXIST_ALREADY)
 	{
 		fd = open(filename, O_WRONLY | O_TRUNC);
 	}
 
 	if (fd == FAILED_TO_OPEN_OR_CREATE_FILE)
 	{
-		return (-1); 
+		return (-1);
 	}
 
 	if (text_content != NULL)
