6d3d6288efb6c0b808f2b51301ba054ee41aefb9
root <rabbitsinwarrens@gmail.com>
2023-12-06 20:26:57 -0800

    feat: add task 0 for hash

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - function
+ * @size: size of hash table
+ * Return: HashTable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = NULL;
+	hash_node_t **array = NULL;
+
+	hash_table = malloc(sizeof(hash_table));
+
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
+	
+	hash_table->array = malloc(sizeof(*array) * size);
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	hash_table->size = size;
+
+	return (hash_table);
+}
