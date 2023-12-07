8b5f63168e22ce20859268f67404f96cdfcaf2ec
root <rabbitsinwarrens@gmail.com>
2023-12-06 22:57:08 -0800

    fix: assign copies to members

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ void prepend(hash_node_t **list, hash_node_t *node)
 
 /**
  * hash_table_set - adds an element to the hash table
- * @ht: hash table
+ * @ht:
  * @key: a key to be indexed to @ht
  * @value: a value to be stored at @key
  * Return: success message.
@@ -27,6 +27,8 @@ int hash_table_set(hash_table_t *ht, char const *key, char const *value)
 {
 	unsigned long int index;
 	hash_node_t *node;
+	char *key_copy;
+	char *value_copy;
 
 	node = malloc(sizeof(*node));
 
@@ -35,8 +37,23 @@ int hash_table_set(hash_table_t *ht, char const *key, char const *value)
 		return (-1);
 	}
 
-	node->key = (char *) key;
-	node->value = (char *) value;
+	key_copy = strdup(key);
+
+	if (!key_copy)
+	{
+		return (-1);
+	}
+
+	value_copy = strdup(value);
+
+	if (!value_copy)
+	{
+		return (-1);
+	}
+
+
+	node->key = (char *) key_copy;
+	node->value = (char *) value_copy;
 	node->next = NULL;
 
 
