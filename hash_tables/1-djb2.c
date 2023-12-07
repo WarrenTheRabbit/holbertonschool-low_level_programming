16261721e68053e35c2b4fc5144ecf651cdd89cb
root <rabbitsinwarrens@gmail.com>
2023-12-06 21:46:45 -0800

    feat: add task 1 for hash

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+
+#include "hash_tables.h"
+
+
+/**
+ * hash_djb2 - function
+ * @str: value to hash
+ * Return: hash result
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = (hash << 5) + hash + c;
+	}
+	return (hash);
+}
