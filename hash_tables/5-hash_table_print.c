5a6502246d522d69295fa28f87e2db6e374badf3
root <rabbitsinwarrens@gmail.com>
2023-12-07 22:51:11 -0800

    style: pass betty

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,37 +10,33 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int size;
 	size_t index;
-	char *key;
-	char *value;
+	char *key, *value;
 	hash_node_t *current_node;
 	int have_printed_an_item;
 
+	if (!ht)
+		return;
 	index = 0;
 	size = ht->size;
 	have_printed_an_item = FALSE;
-
 	printf("%s", "{");
-
-	for (index = 0; index < size; index++)
+	for (index = 0; index < ht->size; index++)
 	{
 		if (ht->array[index])
 		{
 			current_node = ht->array[index];
-
 			while (current_node)
 			{
-	
+
 				key = current_node->key;
 				value = current_node->value;
-	
+
 				if (have_printed_an_item)
 					printf("%s", ", ");
-	
+
 				printf("'%s': '%s'", key, value);
 				have_printed_an_item = TRUE;
-
 				current_node = current_node->next;
 			}
 		}
