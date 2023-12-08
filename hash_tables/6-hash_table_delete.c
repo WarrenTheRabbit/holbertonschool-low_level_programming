5de53708bb2469e089e9a6f29464f295abcf21ff
root <rabbitsinwarrens@gmail.com>
2023-12-07 23:38:01 -0800

    feat: add task 6 for hash

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	size_t index = 0;
+	hash_node_t *current_node, *next_node = NULL;
+
+	if (!ht)
+		return;
+
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index])
+		{
+			current_node = ht->array[index];
+		
+			while (current_node)
+			{
+				next_node = current_node->next;
+				free(current_node->key);
+				free(current_node->value);
+				free(current_node);
+				current_node = next_node;
+			}
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
